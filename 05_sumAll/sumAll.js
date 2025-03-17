2c08b289b8078506e61a6894e054c786f1725c7d
Yahya Arda Özdemir <153550965+YahyaArdaOzdemir@users.noreply.github.com>
2025-03-17 17:13:06 +0300

    complete sum all exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+let sum = 0;
+if(x<0 || y<0){
+    return 'ERROR';
+}
+if(!Number.isInteger(x) || !Number.isInteger(y)){
+    return 'ERROR';
+}
+if(x<y){
+for(i=x; i<=y;i++){
+    sum += i;
+}}
+else{
+    for(i=x; i>=y;i--){
+        sum += i;
+    }
+}
+return sum;
 };
 
 // Do not edit below this line
