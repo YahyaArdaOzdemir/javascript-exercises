339591d3d6700f54deeceda3182c52ea393387f3
Yahya Arda Özdemir <153550965+YahyaArdaOzdemir@users.noreply.github.com>
2025-03-17 17:49:14 +0300

    complete tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celcius = (fahrenheit - 32) / (9/5);
+  if(Number.isInteger(celcius)){
+  return celcius;}
+  else{
+  return +celcius.toFixed(1);
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  let fahrenheit = (celcius * (9/5)) + 32;
+  if(Number.isInteger(fahrenheit)){
+  return fahrenheit;}
+  else{
+    return +fahrenheit.toFixed(1);
+  }
 };
 
 // Do not edit below this line
