bbabc6a7e3ecfc158cf487420e667e09b0530239
Yahya Arda Özdemir <153550965+YahyaArdaOzdemir@users.noreply.github.com>
2026-01-18 16:25:53 +0300

    Fibonacci exercise done.

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    if(a !== +a) a = +a;
+    if(a < 0) return "OOPS";
+    if (a <= 1) return a;
+    let result = 0;
+    let first = 1;
+    let second = 0; 
+    for(let i = 2; i <= a; i++){
+        result = first + second
+        second = first;
+        first = result;
+    }
+    return result;
 };
 
 // Do not edit below this line
