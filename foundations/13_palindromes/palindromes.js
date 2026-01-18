43eee86212fa67483ba9d2079b5d12e8ba23af06
Yahya Arda Özdemir <153550965+YahyaArdaOzdemir@users.noreply.github.com>
2026-01-18 16:04:27 +0300

    Palindromes exercise done.

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,4 +1,9 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    if(word.toLowerCase().split("").reverse()
+    .filter((character) => alphanumerical.includes(character))
+    .join("") === word.toLowerCase().split("").filter((character) => alphanumerical.includes(character)).join("")) return true
+    else return false;
 
 };
 
